5a2467e201434b4e5cf5a1043fdc01a6b02d9964
Alisher <alisner20024@gmail.com>
2024-05-17 15:31:32 +0500

    HomeWork 10 completed

--- src/main/java/org/example/java_core/homework/HomeWork10/CustomLinkedList.java
+++ src/main/java/org/example/java_core/homework/HomeWork10/CustomLinkedList.java
@@ -2,7 +2,6 @@ package org.example.java_core.homework.HomeWork10;
 
 public class CustomLinkedList {
     private Node head;
-
     private int size;
 
     public CustomLinkedList() {
@@ -21,6 +20,20 @@ public class CustomLinkedList {
         } else {
             System.out.println("[]");
         }
+    }
+
+    public void add(int data) {
+        Node node = new Node(data);
+        if(this.head == null) {
+            this.head = node;
+        } else {
+            Node currentNode = head;
+            while (currentNode.getNextNode() != null) {
+                currentNode = currentNode.getNextNode();
+            }
+            currentNode.setNextNode(node);
+        }
+        size++;
     }
 
     public void add(int index, int data) {
@@ -43,8 +56,70 @@ public class CustomLinkedList {
         size++;
     }
 
-    public void remove(int data) {
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
         Node currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+        return currentNode.getData();
+    }
+
+    public int getFirst() {
+        if (head == null) {
+            throw new IllegalStateException("The list is empty");
+        }
+        return head.getData();
+    }
+
+    public int getLast() {
+        if (head == null) {
+            throw new IllegalStateException("The list is empty");
+        }
+        Node currentNode = head;
+        while (currentNode.getNextNode() != null) {
+            currentNode = currentNode.getNextNode();
+        }
+        return currentNode.getData();
+    }
+
+    public boolean contains(int data) {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.getData() == data) {
+                return true;
+            }
+            currentNode = currentNode.getNextNode();
+        }
+        return false;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public int indexOf(int data) {
+        Node currentNode = head;
+        int index = 0;
+        while (currentNode != null) {
+            if (currentNode.getData() == data) {
+                return index;
+            }
+            currentNode = currentNode.getNextNode();
+            index++;
+        }
+        return -1;
+    }
+
+    public void removeByValue(int data) {
+        if (head == null)
+            return;
+
+        Node currentNode = head;
+
         if(currentNode.getData() == data) {
             removeFirst();
             return;
@@ -57,7 +132,22 @@ public class CustomLinkedList {
         }
         currentNode.setNextNode(currentNode.getNextNode().getNextNode());
         size--;
+    }
 
+    public void removeByIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node currentNode = head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.getNextNode();
+            }
+            currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+            size--;
+        }
     }
 
     public void removeFirst() {
@@ -65,17 +155,49 @@ public class CustomLinkedList {
         size--;
     }
 
-    public void add(int data) {
-        Node node = new Node(data);
-        if(this.head == null) {
-            this.head = node;
+    public void removeLast() {
+        if (head == null) {
+            throw new IllegalStateException("The list is empty");
+        }
+        if (size == 1) {
+            head = null;
         } else {
             Node currentNode = head;
-            while (currentNode.getNextNode() != null) {
+            while (currentNode.getNextNode().getNextNode() != null) {
                 currentNode = currentNode.getNextNode();
             }
-            currentNode.setNextNode(node);
+            currentNode.setNextNode(null);
         }
-        size++;
+        size--;
+    }
+
+    public void set(int index, int data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        Node currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+        currentNode.setData(data);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        Node currentNode = head;
+        int index = 0;
+        while (currentNode != null) {
+            array[index++] = currentNode.getData();
+            currentNode = currentNode.getNextNode();
+        }
+        return array;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
--- src/main/java/org/example/java_core/homework/HomeWork10/Main.java
+++ src/main/java/org/example/java_core/homework/HomeWork10/Main.java
@@ -1,5 +1,9 @@
 package org.example.java_core.homework.HomeWork10;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         CustomLinkedList list = new CustomLinkedList();
@@ -8,8 +12,26 @@ public class Main {
         list.add(3);
         list.add(4);
 
-        list.remove(3);
+        list.removeByValue(3);
         list.print();
+
+        list.add(0, 10);
+        System.out.println("Элемент по индексу 2: " + list.get(2));
+        System.out.println("Первый элемент: " + list.getFirst());
+        System.out.println("Последний элемент: " + list.getLast());
+        System.out.println("Содержит ли список значение 5: " + list.contains(5));
+        System.out.println("Индекс элемента со значением 2: " + list.indexOf(2));
+        list.removeByValue(2);
+        list.removeByIndex(0);
+        list.removeFirst();
+        list.removeLast();
+        list.add(15);
+        list.set(0, 20);
+        System.out.println(Arrays.toString(list.toArray()));
+        System.out.println("Размер списка: " + list.size());
+        int[] array = list.toArray();
+        list.clear();
+        System.out.println("Пуст ли список: " + list.isEmpty());
 
         // void add(int index, int data);
         // int get(int index);
@@ -26,7 +48,6 @@ public class Main {
         // int size();
         // int[] toArray();
         // boolean isEmpty()
-
 
     }
 }
